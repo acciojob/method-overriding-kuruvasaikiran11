e2cf2b4e1fb2f5aacce02e79dbe1f8c17137c5a0
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-14 21:56:03 +0530

    Task 3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,16 +1,19 @@
 package com.driver;
 
 
-class A{
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
 
-}
 public class Main {
-  B b = new B();
-  b.meth();
+    class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+    class B extends A{
+
+    }
+    public static void main(String args[]) {
+        B b = new B();
+        b.meth();
+    }
 
 }
