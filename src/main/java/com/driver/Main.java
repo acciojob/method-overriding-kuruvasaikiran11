94384b130cd13beddf4b36762b3e0f0499265268
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2022-12-14 22:08:55 +0530

    Task 4

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -9,7 +9,9 @@ public class Main {
         }
     }
     public static class B extends A{
-
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
     }
     public static void main(String args[]) {
         B b = new B();
